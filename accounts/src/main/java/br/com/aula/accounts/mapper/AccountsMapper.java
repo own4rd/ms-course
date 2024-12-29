264ed186176af9e95f486355669b6e5a1ff6229f
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-12-29 12:41:29 -0300

    adding accounts ms

---
+++ accounts/src/main/java/br/com/aula/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package br.com.aula.accounts.mapper;
+
+import br.com.aula.accounts.dto.AccountsDto;
+import br.com.aula.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
---
+++ accounts/src/main/java/br/com/aula/accounts/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package br.com.aula.accounts.repository;
+
+import br.com.aula.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+}
